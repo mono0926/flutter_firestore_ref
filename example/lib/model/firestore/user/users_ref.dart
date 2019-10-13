c8e74a7c601acb3739f86d9740264bbd6c9e9127
Masayuki Ono <mono0926@gmail.com>
2019-10-13 09:37:23 +0900

    Refactor

--- example/lib/model/firestore/user/users_ref.dart
+++ example/lib/model/firestore/user/users_ref.dart
@@ -1,5 +1,4 @@
 import 'package:firestore_ref/firestore_ref.dart';
-import 'package:flutter/foundation.dart';
 
 import 'user.dart';
 import 'user_doc.dart';
@@ -8,23 +7,12 @@ import 'user_ref.dart';
 export 'user_ref.dart';
 
 class UsersRef extends CollectionRef<User, UserDoc> {
-  const UsersRef._({
-    @required CollectionReference ref,
-    @required DocumentDecoder<UserDoc> decoder,
-    @required EntityEncoder<User> encoder,
-  }) : super(
-          ref: ref,
-          decoder: decoder,
-          encoder: encoder,
+  UsersRef.ref()
+      : super(
+          ref: collectionRef(collection),
+          decoder: _UserDocDecoder(),
+          encoder: _UserEncoder(),
         );
-
-  factory UsersRef.ref() {
-    return UsersRef._(
-      ref: collectionRef(collection),
-      decoder: _UserDocDecoder(),
-      encoder: _UserEncoder(),
-    );
-  }
 
   static const collection = 'users';
 
@@ -51,7 +39,5 @@ class _UserDocDecoder extends DocumentDecoder<UserDoc> {
 
 class _UserEncoder extends EntityEncoder<User> {
   @override
-  Map<String, dynamic> encode(User entity) {
-    return entity.toJson();
-  }
+  Map<String, dynamic> encode(User entity) => entity.toJson();
 }
