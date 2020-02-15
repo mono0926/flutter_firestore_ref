43ae513dd8b0076907fe69fce26859c49b4b7675
Masayuki Ono <mono0926@gmail.com>
2020-02-15 16:47:53 +0900

    Refactor

--- example/lib/model/firestore/user/user.dart
+++ example/lib/model/firestore/user/user.dart
@@ -3,9 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-export 'user_doc.dart';
-export 'users_ref.dart';
-
 part 'user.freezed.dart';
 part 'user.g.dart';
 
@@ -25,4 +22,52 @@ extension UserEx on User {
         json: toJson(),
         createdAt: createdAt,
       );
+}
+
+class UserDoc extends Document<User> {
+  const UserDoc(
+    String id,
+    User entity,
+  ) : super(
+          id,
+          entity,
+        );
+}
+
+class UserRef extends DocumentRef<User, UserDoc> {
+  const UserRef({
+    @required DocumentReference ref,
+    @required DocumentDecoder<UserDoc> decoder,
+    @required EntityEncoder<User> encoder,
+  }) : super(
+          ref: ref,
+          decoder: decoder,
+          encoder: encoder,
+        );
+}
+
+class UsersRef extends CollectionRef<User, UserDoc> {
+  UsersRef.ref()
+      : super(
+          ref: Firestore.instance.collection(collection),
+          decoder: (snap) => UserDoc(
+            snap.documentID,
+            User.fromJson(snap.data),
+          ),
+          encoder: (entity) => HasTimestamp.replacingTimestamp(
+            json: entity.toJson(),
+            createdAt: entity.createdAt,
+          ),
+        );
+
+  static const collection = 'users';
+
+  @override
+  UserRef docRef([String id]) {
+    return UserRef(
+      ref: docRefRaw(id),
+      encoder: encoder,
+      decoder: decoder,
+    );
+  }
 }
