3c0636b9d29c09ea381ff6e5f90d44ebed2367f0
Masayuki Ono <mono0926@gmail.com>
2020-03-02 13:50:38 +0900

    Refactor by using JsonKey

--- lib/src/json_converter.dart
+++ lib/src/json_converter.dart
@@ -1,6 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+class PassthroughConverter<T> implements JsonConverter<T, T> {
+  const PassthroughConverter();
+
+  @override
+  T fromJson(T json) => json;
+
+  @override
+  T toJson(T object) => object;
+}
+
 class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
   const TimestampConverter();
 
@@ -12,43 +22,23 @@ class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
       object == null ? null : Timestamp.fromDate(object);
 }
 
-Set<DocumentReference> documentReferenceSetFromListValue(dynamic value) {
-  return value == null
-      ? null
-      : Set.from((value as List).cast<DocumentReference>());
+class DocumentReferenceSetConverter
+    extends JsonConverter<Set<DocumentReference>, List<DocumentReference>> {
+  @override
+  Set<DocumentReference> fromJson(List<DocumentReference> json) =>
+      Set.from(json);
+
+  @override
+  List<DocumentReference> toJson(Set<DocumentReference> object) =>
+      List.from(object);
 }
 
-List<DocumentReference> documentReferenceListFromSetValue(dynamic value) {
-  return value == null
-      ? null
-      : List.from((value as Set).cast<DocumentReference>());
+class DocumentReferenceListConverter
+    extends PassthroughConverter<List<DocumentReference>> {
+  const DocumentReferenceListConverter();
 }
-
-List<DocumentReference> documentReferenceListFromListValue(dynamic value) {
-  return value == null ? null : (value as List).cast();
-}
-
-const documentReferenceSetJsonKey = JsonKey(
-  fromJson: documentReferenceSetFromListValue,
-  toJson: documentReferenceListFromSetValue,
-);
-
-const documentReferenceListJsonKey = JsonKey(
-  fromJson: documentReferenceListFromListValue,
-  toJson: documentReferenceListFromListValue,
-);
 
 class DocumentReferenceConverter
     extends PassthroughConverter<DocumentReference> {
   const DocumentReferenceConverter();
-}
-
-class PassthroughConverter<T> implements JsonConverter<T, T> {
-  const PassthroughConverter();
-
-  @override
-  T fromJson(T json) => json;
-
-  @override
-  T toJson(T object) => object;
 }
