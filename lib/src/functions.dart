bb68c1a6cfbfdba718bfca82df5d0ddf46fac5a9
Masayuki Ono <mono0926@gmail.com>
2020-03-25 12:40:17 +0900

    Delete executeBatch, which was added mistakenly

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -2,10 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_ref/firestore_ref.dart';
 import 'package:meta/meta.dart';
 
-Future<void> runBatchWrite(Future Function(WriteBatch batch) f) async {
+Future<T> runBatchWrite<T>(Future<T> Function(WriteBatch batch) f) async {
   final batch = Firestore.instance.batch();
-  await f(batch);
+  final result = await f(batch);
   await batch.commit();
+  return result;
 }
 
 Map<String, dynamic> parseJson(
@@ -23,11 +24,4 @@ T parse<T>(
 }) {
   final parsedJson = parseJson(json, key: key);
   return parsedJson == null ? null : fromJson(parsedJson);
-}
-
-Future<T> executeBatch<T>(Future<T> Function(WriteBatch batch) f) async {
-  final batch = Firestore.instance.batch();
-  final result = await f(batch);
-  await batch.commit();
-  return result;
 }
