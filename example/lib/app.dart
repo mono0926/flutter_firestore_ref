9faad3e7be5a62121592dc3e0876107b9d12baf7
Masayuki Ono <mono0926@gmail.com>
2020-05-22 12:48:15 +0900

    Wait for authed

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:example/model/service/authenticator.dart';
 import 'package:example/util/util.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,11 @@ import 'run.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: context.select((AppInfo info) => info.title),
-      home: HomePage.wrapped(),
-    );
+    return context.select((Authenticator a) => a.user == null)
+        ? const Center(child: CircularProgressIndicator())
+        : MaterialApp(
+            title: context.select((AppInfo info) => info.title),
+            home: HomePage.wrapped(),
+          );
   }
 }
