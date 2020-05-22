27dedc4798d12358afacd4b0f5364a16062b9bbd
Masayuki Ono <mono0926@gmail.com>
2020-05-22 10:48:38 +0900

    Support emulator

--- example/lib/run.dart
+++ example/lib/run.dart
@@ -1,16 +1,30 @@
+import 'dart:io';
+
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firestore_ref/firestore_ref.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'model/service/service.dart';
 import 'util/util.dart';
 
-void run({bool isEmulator = false}) {
+Future<void> run({bool isEmulator = false}) async {
   logger.fine('start(isEmulator: $isEmulator)');
   WidgetsFlutterBinding.ensureInitialized();
-  // For web
-  Firestore.instance.settings(persistenceEnabled: true);
-  // TODO(mono): Emulator対応
+
+  if (isEmulator) {
+    final domain = (!kIsWeb && Platform.isAndroid) ? '10.0.2.2' : 'localhost';
+    CloudFunctions.instance.useFunctionsEmulator(origin: 'http://$domain:5001');
+    await Firestore.instance.settings(
+      persistenceEnabled: false,
+      host: '$domain:8080',
+      sslEnabled: false,
+    );
+  } else if (kIsWeb) {
+    await Firestore.instance.settings(persistenceEnabled: true);
+  }
+
   runApp(
     MultiProvider(
       providers: [
