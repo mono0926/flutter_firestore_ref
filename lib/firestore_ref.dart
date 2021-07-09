b7436f97eb1b57e3497a4ef62584b08bf36edb2b
Masayuki Ono <mono0926@gmail.com>
2021-07-09 15:07:50 +0900

    Delete useFirestoreEmulator in favor of FirebaseFirestore.instance.useFirestoreEmulator

--- lib/firestore_ref.dart
+++ lib/firestore_ref.dart
@@ -5,7 +5,6 @@ export 'package:cloud_firestore/cloud_firestore.dart';
 export 'src/collection_ref.dart';
 export 'src/document.dart';
 export 'src/document_ref.dart';
-export 'src/emulator.dart';
 export 'src/firestore_operation_counter.dart';
 export 'src/functions.dart';
 export 'src/json_converter.dart';
